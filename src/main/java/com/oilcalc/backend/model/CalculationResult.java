f518f47c8c26d2dd0fa8876cc4a73bb0bee8e3c3
Shahir Jalal <shahirjalalhazli@gmail.com>
2025-03-25 00:35:32 +0800

    Start

---
+++ src/main/java/com/oilcalc/backend/model/CalculationResult.java
@@ -0,0 +1,30 @@
+package com.oilcalc.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CalculationResult {
+    private double pressureAtToolstring;
+    private double surfaceWaterFlowrate;
+    private double netForceOnWire;
+    private double downholeDensityGas;
+    private double netForceOfToolstring;
+    private double netForceOfSystem;
+    private double lineWeightDeviatedLessBuoyancy;
+    private double toolWeightDeviatedLessBuoyancy;
+    private double linePullAtSurface;
+    private double wellheadPress;
+    private double pressControlFriction;
+    private double dragOrLiftOnLine;
+    private double dragOrLiftOnTools;
+    private double positiveDownwardForces;
+    private double counterForces;
+    private boolean dragOnLinePositive;
+    private boolean dragOnToolsPositive;
+}
